ba8883e7a184ebaeae35b080503891464775768a
skane88 <sean.kane@outlook.com.au>
2017-01-02 12:18:34 +1000

    Added S5.2 section capacity method

--- AS4100 Modules/AS4100_5.py
+++ AS4100 Modules/AS4100_5.py
@@ -21,3 +21,15 @@ Note that this contradicts current Australian Practice which uses kN, MPa etc.
 However conversion is simple in most cases because the formulas are written
 in consistent systems of units.
 '''
+
+#section capacity methods
+
+def s5_2_Ms(f_y, Z_e):
+    '''
+    Calculates the member section capacity according to AS4100 S5.2
+    
+    f_y: the section yield stress.
+    Ze: the effective section modulus calculated according to S5.2.
+    '''
+    
+    return fy * Ze
