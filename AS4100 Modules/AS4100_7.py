b5d570c95b00eb2f4a6a59e13e9c88143c616a08
skane88 <sean.kane@outlook.com.au>
2017-06-25 21:16:03 +1000

    Updated method descriptions in AS4100_7.

--- AS4100 Modules/AS4100_7.py
+++ AS4100 Modules/AS4100_7.py
@@ -4,135 +4,159 @@ It includes helper functions for determining if pin
 connections comply with clause 7.5 as well
 '''
 
-def s7_2_Tensile_Yield(A_g, f_y):
+def s7_2_Tensile_Yield(A_g: float, f_y: float) -> float:
     '''
-    Calculates the yield capacity of a member
-    but does not include the capacity reduction factor
+    Calculates the yield capacity of a member but does not include the capacity
+    reduction factor.
     
-    A_g: gross area of a section
-    f_y: yield strength of a section
+    :param A_g: Gross area of a section in m².
+    :param f_y: Yield strength of a section in Pa.
+    :return: Returns the yielding capacity of the member in N.
     '''
 
     return A_g * f_y
 
-def s7_2_Area_Reqd_Yield(N_t, f_y):
+def s7_2_Area_Reqd_Yield(N_t: float, f_y: float) -> float:
     '''
-    Calculates the area required to carry the force applied without
-    yielding.
+    Calculates the area required to carry the force applied without yielding.
     
-    N_t: applied force
-    f_y: yield strength of a section
+    :param N_t: The applied force in N.
+    :param f_y: The yield strength of a section in Pa.
+    :return: Returns the area required to carry the force applied without
+        yielding (in m²).
     '''
 
     return N_t / f_y
 
-def s7_2_Ultimate_Fracture(A_n, f_u, k_t, ultimate_uncertainty = 0.85):
+def s7_2_Ultimate_Fracture(A_n: float, f_u: float, k_t: float,
+                           ultimate_uncertainty: float = 0.85) -> float:
     '''
-    Calculates the ultimate fracture capacity of a section
-    and includes the additional uncertainty factor from AS4100.
+    Calculates the ultimate fracture capacity of a section and includes the
+    additional uncertainty factor from AS4100.
     
-    A_n: net area of a section
-    f_u: ultimate strength of a section
-    k_t: eccentric connection factor
-    ultimate_uncertainty: an uncertainty factor for ultimate
-        strength behaviour, by default 0.85.
+    :param A_n: Net area of the section in m², allowing for holes as required
+        by AS4100.
+    :param f_u: The ultimate strength of the section in Pa.
+    :param k_t: The connection efficiency factor / eccentric connection factor
+        as per AS4100.
+    :param ultimate_uncertainty: An uncertainty factor for ultimate strength
+        behaviour, by default 0.85.
+    :return: Returns the ultimate fracture capacity in N.
     '''
     
     return A_n * f_u * k_t * ultimate_uncertainty
 
-def s7_2_Area_Reqd_Ultimate(N_t, f_u, k_t, ultimate_uncertainty = 0.85):
+def s7_2_Area_Reqd_Ultimate(N_t: float, f_u: float, k_t: float,
+                            ultimate_uncertainty: float = 0.85) -> float:
     '''
-    Calculates the area required to carry the force applied without
-    Fracture at ultimate load.
+    Calculates the area required to carry the force applied without fracture at
+    ultimate load.
     
-    N_t: applied force
-    f_u: ultimate strength of a section
-    k_t: eccentric connection factor
-    ultimate_uncertainty: an uncertainty factor for ultimate
+    :param N_t: The applied force in N.
+    :param f_u: Ultimate strength of the section in Pa.
+    :param k_t: The connection efficiency factor / eccentric connection factor
+        as per AS4100.
+    :param ultimate_uncertainty: an uncertainty factor for ultimate
         strength behaviour, by default 0.85.
     '''
 
     return N_t / ultimate_uncertainty / k_t / f_u
 
-def s7_5_a_Unstiffened_Thickness(a_e, t):
+def s7_5_a_Unstiffened_Thickness(a_e: float, t: float) -> bool:
     '''
-    Determines if the thickness satisfies the requirements
-    of AS4100 S7.5a).
-    
-    a_e: edge distance from edge of hole to edge of member
-    t: thickness of member.
-    
+    Determines if the thickness of a pinned connection satisfies the
+    requirements of AS4100 S7.5a.
+
     Note that this cannot be an exhaustive check as it depends
     significantly on the connection geometry which this library
     is not intended to check.
+
+    :param a_e: Edge distance from edge of hole to edge of member in m.
+    :param t: Thickness of member in m.
+    :returns: Does the thickness of the connection comply with AS4100 S7.5a
+        (True or False).
     '''
 
     if t > 4 * a_e:
         return True
     return False
 
-def s7_5_b_Area_Beyond_Hole(A_reqd, A_n):
+def s7_5_b_Area_Beyond_Hole(A_reqd: float, A_n: float) -> bool:
     '''
-    Determines if the area beyond the hole complies with
-    the requirements of AS4100 S7.5b)
-    
-    A_reqd: Area required for a member to carry the tension load
-        based on AS4100 S7.2.
-    A_n: the net area beyond the hole or within 45deg.
+    Determines if the area beyond the hole in a pinned connection complies with
+    the requirements of AS4100 S7.5b.
+
+    Note that this cannot be an exhaustive check as it depends
+    significantly on the connection geometry which this library
+    is not intended to check.
+
+    :param A_reqd: Area required for a member to carry the tension load in m².
+        Based on AS4100 S7.2.
+    :param A_n: The net area beyond the hole or within 45deg, in m².
+    :return: Does the connection comply with AS4100 S7.5b (True or False).
     '''
 
     if A_n > A_reqd:
         return True
     return False
 
-def s7_5_c_Area_Perpendicular_Hole(A_reqd, A_sum):
+def s7_5_c_Area_Perpendicular_Hole(A_reqd: float, A_sum: float) -> bool:
     '''
-    Determines if the area perpendicular to the hole complies with
-    the requirements of AS4100 S7.5c)
+    Determines if the area perpendicular to the hole in a pinned connection
+        complies with the requirements of AS4100 S7.5c.
     
-    A_reqd: Area required for a member to carry the tension load
-        based on AS4100 S7.2.
-    A_sum: the sum of the area perpendicular to the member axis.
+    :param A_reqd: Area required for a member to carry the tension load in m².
+        Based on AS4100 S7.2.
+    :param A_sum: The sum of the area perpendicular to the member axis in m².
+    :return: Does the connection comply with AS4100 S7.5c (True or False).
     '''
 
     if A_sum > 1.33*A_reqd:
         return True
     return False
 
-def s7_1_Area_Reqd(N_t, f_y, f_u, k_t, φ = 0.9, ultimate_uncertainty = 0.85):
+def s7_1_Area_Reqd(N_t: float, f_y: float, f_u: float, k_t: float,
+                   φ: float = 0.9, ultimate_uncertainty: float = 0.85) -> float:
     '''
     Calculates the area required for a member to AS4100 section 7.1
     
-    N_t: the design force.
-    f_y: the yield strength of the section
-    f_u: the ultimate strength of the section
-    k_t: the connection efficiency factor
-    φ: the capacity reduction factor, by default 0.9
-    ultimate_uncertainty: a factor for the uncertainty in ultimate strength
-    as per AS4100 S7.2. By default 0.85.
+    :param N_t: The applied force in N.
+    :param f_y: The yield strength of a section in Pa.
+    :param f_u: The ultimate strength of the section in Pa.
+    :param k_t: The connection efficiency factor / eccentric connection factor
+        as per AS4100.
+    :param φ: The capacity reduction factor, by default 0.9.
+    :param ultimate_uncertainty: A factor for the uncertainty in ultimate
+        strength as per AS4100 S7.2. By default 0.85.
+    :return: Returns the area required to carry the applied load without yield
+        or fracture as per A4100, in m².
     '''
 
-    return max(s7_2_Area_Reqd_Ultimate(N_t,f_u,k_t,ultimate_uncertainty),
-               s7_2_Area_Reqd_Yield(N_t,f_y))/φ
+    return max(s7_2_Area_Reqd_Ultimate(N_t, f_u, k_t, ultimate_uncertainty),
+               s7_2_Area_Reqd_Yield(N_t, f_y)) / φ
 
-def s7_1_Tension_Capacity(A_g, A_n, f_y, f_u, k_t, φ = 0.9, 
-                          ultimate_uncertainty = 0.85):
+def s7_1_Tension_Capacity(A_g: float, A_n: float, f_y: float, f_u: float,
+                          k_t: float, φ: float = 0.9,
+                          ultimate_uncertainty: float = 0.85) -> float:
     '''
     Calculates the tension capacity of a section according to AS4100 S7.1.
     
-    A_g: gross section area
-    A_n: net section area
-    f_y: yield strength. If different components have different yield
-         strengths the minimum strength of the section should be used.
-         Where a section has a significantly different strength the result
-         may be too conservative and this function may not be appropriate
-         (i.e. a 250 grade web and a 450 grade flange).
-    f_u: ultimate strength
-    k_t: connection efficiency factor.
-    φ: Capacity reduction factor, default is 0.9
-    ultimate_uncertainty: a factor for uncertainty about the ultimate
-                          strength of a section,
-    default is 0.85 as per AS4100 S7.2.
+    :param A_g: Gross area of a section in m².
+    :param A_n: Net area of the section in m², allowing for holes as required
+        by AS4100.
+    :param f_y: The yield strength of the section in Pa. If different components
+        have different yield strengths the minimum strength of the section
+        should be used. Where a section has a significantly different strength
+        the result may be too conservative and this function may not be
+        appropriate (i.e. a 250 grade web and a 450 grade flange) - more
+        detailed analysis (FEA modelling etc.) may be required.
+    :param f_u: The ultimate strength of the section in Pa.
+    :param k_t: The connection efficiency factor / eccentric connection factor
+        as per AS4100.
+    :param φ: The capacity reduction factor, by default 0.9.
+    :param ultimate_uncertainty: A factor for the uncertainty in ultimate
+        strength as per AS4100 S7.2. By default 0.85.
+    :return: Returns the tensile strength of the section as per AS4100 S7.1.
     '''
 
     return φ * min(s7_2_Tensile_Yield(A_g, f_y), 
