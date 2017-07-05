8e1a59b559d4cf0ea9d8f1e82d9818f7af233ad7
skane88 <sean.kane@outlook.com.au>
2017-07-05 11:23:33 +1000

    Updated unit tests for AS4100_7.

--- AS4100 Modules/AS4100_7.py
+++ AS4100 Modules/AS4100_7.py
@@ -81,29 +81,28 @@ def s7_2_An(N_t: float, f_u: float, k_t: float,
 
     return N_t / ultimate_uncertainty / k_t / f_u
 
-def s7_5_a_Unstiffened_Thickness(a_e: float, t: float) -> bool:
+def s7_5_a_min_thickness(a_e: float, safety_factor: 0.25) -> float:
     '''
-    Determines if the thickness of a pinned connection satisfies the
-    requirements of AS4100 S7.5a.
+    Determines the required thickness of a connection plate in a pinned
+    connection as per AS4100 S7.5a).
 
     Note that this cannot be an exhaustive check as it depends
     significantly on the connection geometry which this library
     is not intended to check.
 
     :param a_e: Edge distance from edge of hole to edge of member in m.
-    :param t: Thickness of member in m.
-    :returns: Does the thickness of the connection comply with AS4100 S7.5a
-        (True or False).
+    :param safety_factor: The required minimum safety factor as per
+        AS4100 S7.5a). By default 0.25 as per AS4100.
+    :returns: The required thickness of the connection plate in m.
     '''
 
-    if t > 4 * a_e:
-        return True
-    return False
+    return a_e * safety_factor
 
-def s7_5_b_Area_Beyond_Hole(A_reqd: float, A_n: float) -> bool:
+def s7_5_b_A_beyond(A_reqd: float, safety_factor: float = 1.00) \
+        -> float:
     '''
-    Determines if the area beyond the hole in a pinned connection complies with
-    the requirements of AS4100 S7.5b.
+    Determines the area required beyond a hole in a pinned connection based on
+        the requirements of AS4100 S7.5.b).
 
     Note that this cannot be an exhaustive check as it depends
     significantly on the connection geometry which this library
@@ -111,28 +110,27 @@ def s7_5_b_Area_Beyond_Hole(A_reqd: float, A_n: float) -> bool:
 
     :param A_reqd: Area required for a member to carry the tension load in m².
         Based on AS4100 S7.2.
-    :param A_n: The net area beyond the hole or within 45deg, in m².
-    :return: Does the connection comply with AS4100 S7.5b (True or False).
+    :param safety_factor: The safety factor on the member area as per AS4100
+        S7.5b). By default 1.00 as per AS4100.
+    :return: The area required beyond the hole in m².
     '''
 
-    if A_n > A_reqd:
-        return True
-    return False
+    return A_reqd * safety_factor
 
-def s7_5_c_Area_Perpendicular_Hole(A_reqd: float, A_sum: float) -> bool:
+def s7_5_c_A_perp(A_reqd: float, safety_factor: float = 1.33) \
+        -> float:
     '''
-    Determines if the area perpendicular to the hole in a pinned connection
-        complies with the requirements of AS4100 S7.5c.
+    Determines the area required at a pinned connection, perpendicular to the
+        member axis based on the requirements of AS4100 S7.5c.
     
     :param A_reqd: Area required for a member to carry the tension load in m².
         Based on AS4100 S7.2.
-    :param A_sum: The sum of the area perpendicular to the member axis in m².
-    :return: Does the connection comply with AS4100 S7.5c (True or False).
+    :param safety_factor The safety factor against the member area as given in
+        AS4100 S7.5c). By default 1.33 as per AS4100..
+    :return: The area required perpendicular to the axis of the member, in m².
     '''
 
-    if A_sum > 1.33*A_reqd:
-        return True
-    return False
+    return A_reqd * safety_factor
 
 def s7_1_A_reqd(N_t: float, f_y: float, f_u: float, k_t: float,
                    φ: float = 0.9, ultimate_uncertainty: float = 0.85) -> Dict[
@@ -171,9 +169,8 @@ def s7_1_A_reqd(N_t: float, f_y: float, f_u: float, k_t: float,
         }
     '''
 
-    results = {'A_g': s7_2_Area_Reqd_Ultimate(N_t, f_u, k_t,
-                                              ultimate_uncertainty),
-               'A_n': s7_2_Area_Reqd_Yield(N_t, f_y)}
+    results = {'A_g': s7_2_Ag(N_t, f_y),
+               'A_n': s7_2_An(N_t, f_u, k_t, ultimate_uncertainty)}
 
     results['A'] = max(results.values())
 
