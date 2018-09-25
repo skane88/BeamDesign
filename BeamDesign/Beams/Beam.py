ae47a999bc09784a83a12899e760af7554dd99d4
skane88 <sean.kane@outlook.com.au>
2018-09-25 15:34:25 +1000

    Began simple Beam class.

---
+++ BeamDesign/Beams/Beam.py
@@ -0,0 +1,93 @@
+"""
+This file is intended to define a class that will describe beam objects. These
+are intended to represent real world structural elements and as such their
+properties will be limited to those that are explicitly real world properties,
+such as:
+
+* Length
+* Cross section
+* Material Properties
+* Internal loads (i.e. bending moments etc.). At this point in time it is NOT
+intended to include descriptions of applied loads and / or to calculate
+load diagrams directly, although a sub-class may be created eventually to allow
+this.
+
+Information that is design code specific will not be stored. This includes
+(but is not limited to):
+
+* Capacity reduction factors, safety factors
+* Location and types of restraints in compression / bending etc.
+* Capacity ratios / strength calculations etc.
+
+The intent here is to make the ``Beam`` class as generic as possible for use
+with multiple design codes.
+"""
+
+import numpy as np
+
+
+class Beam:
+
+    def __init__(self, *, length, section=None, material=None, name: str=None,
+                 loads=None, **kwargs):
+        self.length = length
+        self.section = section
+        self.material = material
+        self.loads = loads
+
+        # if name is specified, pop off name and give it to the beam element
+        self.name = name
+
+        for i, v in kwargs.items():
+
+            if i in self.__dict__:
+                raise ValueError(f'Argument {i} already present in the beam '
+                                 + f'object.')
+
+            self.__dict__[i] = v
+
+    @property
+    def loads(self):
+        return self._loads
+
+    @loads.setter
+    def loads(self, loads: None):
+        loads = np.array(loads)
+        self._loads = loads
+
+    def get_loads(self, position):
+
+        return [self.get_load_x(position),
+                self.get_load_y(position),
+                self.get_load_z(position),
+                self.get_load_mx(position),
+                self.get_load_my(position),
+                self.get_load_mz(position)]
+
+    def get_load_x(self, position):
+
+        return self.get_load_generic(position, 'x')
+
+    def get_load_y(self, position):
+
+        return self.get_load_generic(position, 'y')
+
+    def get_load_z(self, position):
+
+        return self.get_load_generic(position, 'z')
+
+    def get_load_mx(self, position):
+
+        return self.get_load_generic(position, 'mx')
+
+    def get_load_my(self, position):
+
+        return self.get_load_generic(position, 'my')
+
+    def get_load_mz(self, position):
+
+        return self.get_load_generic(position, 'mz')
+
+    def get_load_generic(self, *,  position, load: str):
+
+        raise NotImplementedError()
