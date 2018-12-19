11d7e786016811c80ef52d846076d66b908e4d3b
skane88 <sean.kane@outlook.com.au>
2018-12-19 21:01:55 +1000

    Ran Black on files.

--- BeamDesign/Sections/Circle.py
+++ BeamDesign/Sections/Circle.py
@@ -5,20 +5,20 @@ for calculating section properties. It inherits from
 from BeamDesign.Sections.Section import Section
 import math
 
+
 class Circle(Section):
     """
     CircleClass implements a class for  calculating
     section properties of a circular section.
     """
 
-    def __init__(self, x = 0., y = 0., radius = 0.):
+    def __init__(self, x=0.0, y=0.0, radius=0.0):
         """
         Initialise the Circle object.
 
-        x: the x co-ordinate of the centre of the shape. Default is 0.
-        y: the y co-ordinate of the centre of the shape. Default is 0.
-        radius_i: the interior radius of the shape. Default is 0.
-        radius_o: the exterior radius of the shape. Default is 0.
+        :param x: the x co-ordinate of the centre of the shape.
+        :param y: the y co-ordinate of the centre of the shape.
+        :param radius: the radius of the shape.
         """
 
         super().__init__(x, y)
--- BeamDesign/Sections/HollowCircle.py
+++ BeamDesign/Sections/HollowCircle.py
@@ -10,13 +10,13 @@ class HollowCircle(Section):
     section properties of a hollow circular section.
     """
 
-    def __init__(self, x=0.0, y=0.0, radius_o=0., radius_i=0.):
+    def __init__(self, x=0.0, y=0.0, radius_o=0.0, radius_i=0.0):
         """Initialise the HollowCircle object.
         
-        x: the x co-ordinate of the centre of the shape. Default is 0.
-        y: the y co-ordinate of the centre of the shape. Default is 0.
-        radius_o: the exterior radius of the shape. Default is 0.
-        radius_i: the interior radius of the shape. Default is 0."""
+        x: the x co-ordinate of the centre of the shape.
+        y: the y co-ordinate of the centre of the shape.
+        radius_o: the exterior radius of the shape.
+        radius_i: the interior radius of the shape."""
 
         super().__init__(x, y)
         self.radius_o = radius_o
@@ -31,14 +31,13 @@ class HollowCircle(Section):
         return self._radius_i
 
     @radius_i.setter
-    def radius_i(self, radius_i=0.):
+    def radius_i(self, radius_i=0.0):
         # A setter function to catch the error of the
         # inner radius being larger than the outer radius.
 
         if radius_i > self.radius_o:
             # test for the inner radius being larger than the outer.
-            raise ValueError(
-                "inner radius should be smaller than the outer radius")
+            raise ValueError("inner radius should be smaller than the outer radius")
         else:
             self._radius_i = radius_i
 
