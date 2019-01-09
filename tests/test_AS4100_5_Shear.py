dff27f14ba3a44de8d2849cea316b87e67341a29
skane88 <sean.kane@outlook.com.au>
2019-01-09 20:59:15 +1000

    Updating the tests for shear methods.

--- tests/test_AS4100_5_Shear.py
+++ tests/test_AS4100_5_Shear.py
@@ -1,24 +1,32 @@
-from unittest import TestCase
+"""
+Contains tests for the shear calculation methods.
+"""
 
 
-class Test_AS4100_S5_Shear(TestCase):
-    def test_s5_11_4_V_w_Generic(self):
-        self.fail()
+def test_s5_11_4_V_w_Generic():
 
-    def Test_s5_11_4_V_w_WeldInterfaceLimited(self):
-        self.fail()
+    assert False
 
-    def Test_s5_11_4_V_w_WeldLimited(self):
-        self.fail()
 
-    def Test_s5_11_4_V_w_CHS(self):
-        self.fail()
+def test_s5_11_4_V_w_CHS():
 
-    def Test_s5_11_5_α_v(self):
-        self.fail()
+    assert False
 
-    def Test_s5_11_3_Non_uniform_shear_factor(self):
-        self.fail()
 
-    def Test_s5_11_2_V_u(self):
-        self.fail()
+def test_s5_11_5_α_v():
+
+    assert False
+
+
+def test_s5_11_3_Non_uniform_shear_factor():
+
+    assert False
+
+
+def test_s5_11_4_V_w_Weld():
+    assert False
+
+
+def test_s5_11_4_V_w_Interface():
+
+    assert False
