075623773cfce38c608024d11464c8ce2592685b
skane88 <sean.kane@outlook.com.au>
2019-01-10 09:27:51 +1000

    Working on tests for S5_Shear.

--- tests/test_AS4100_5_Shear.py
+++ tests/test_AS4100_5_Shear.py
@@ -2,10 +2,36 @@
 Contains tests for the shear calculation methods.
 """
 
+from math import isclose
 
-def test_s5_11_4_V_w_Generic():
+import pytest
+import pandas as pd
 
-    assert False
+import BeamDesign.Code_Check.AS4100.S5_Shear as S5_Shear
+
+
+def data_WB():
+
+    df = pd.read_excel(
+        r"Excel\AS4100_5_Shear Verification.xlsx",
+        sheet_name="Welded Beams",
+        index_col=0,
+        usecols="B:AA",
+    )
+
+    return [(row.Index, row) for row in df.itertuples()]
+
+
+@pytest.mark.parametrize("name, data", data_WB())
+def test_s5_11_4_V_w_Generic(name, data):
+
+    Aw = data.Aw
+    fy = data.fy
+
+    expected = data.φVyield
+    actual = S5_Shear.s5_11_4_V_w_Generic(A_w=Aw, f_y=fy)
+
+    assert isclose(expected, actual)
 
 
 def test_s5_11_4_V_w_CHS():
