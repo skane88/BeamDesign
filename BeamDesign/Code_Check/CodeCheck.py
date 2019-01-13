0438b1d7909a9a970756c98b7879e0f4181e8241
skane88 <sean.kane@outlook.com.au>
2019-01-14 09:05:46 +1000

    Added some properties to the Abstract CodeCheck class.
    Added __init__ method and added some properties to add a Beam & Section.

--- BeamDesign/Code_Check/CodeCheck.py
+++ BeamDesign/Code_Check/CodeCheck.py
@@ -4,8 +4,24 @@ This will contain an Abstract Base Class that all CodeCheck classes should inher
 
 from abc import ABC, abstractmethod
 
+from BeamDesign.Beam import Beam
+
 
 class CodeCheck(ABC):
+    def __init__(self, beam: Beam, section):
+
+        self._beam = beam
+        self._section = section
+
+    @property
+    def beam(self) -> Beam:
+        return self._beam
+
+    @property
+    def section(self):
+        return self._section
+
+    @property
     @abstractmethod
     def tension_capacity(self) -> float:
         """
@@ -15,3 +31,11 @@ class CodeCheck(ABC):
         """
 
         raise NotImplementedError()
+
+    @abstractmethod
+    def section(self, position: float=None):
+
+        if self.beam is None:
+            return self.section
+        else:
+            raise self.beam.get_section(position=position)
