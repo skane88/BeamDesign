1fe3f34f63fde3738de80f88c8f3b824dcbf4da0
skane88 <sean.kane@outlook.com.au>
2019-01-14 09:05:00 +1000

    Started to work on the AS4100 check.
    Defined an empty AS4100 class.

---
+++ BeamDesign/Code_Check/AS4100/AS4100.py
@@ -0,0 +1,13 @@
+"""
+This file is intended to contain a class for AS4100 checks.
+"""
+
+from BeamDesign.Code_Check.CodeCheck import CodeCheck
+
+class AS4100(CodeCheck):
+
+    def __init__(self, beam, section):
+
+        super().__init__(beam=beam, section=section)
+
+    pass
