05d1ba7e81eeff867d7338b66f9d5bf3d8183827
skane88 <sean.kane@outlook.com.au>
2019-01-17 21:03:20 +1000

    Updated test_AS4100_5_Shear.py to use test_utils.

--- tests/test_AS4100_5_Shear.py
+++ tests/test_AS4100_5_Shear.py
@@ -9,82 +9,9 @@ import pandas as pd
 
 import BeamDesign.CodeCheck.AS4100.S5_Shear as S5_Shear
 
+from tests.test_utils import *
 
-def data_Is():
-
-    df = pd.read_excel(
-        r"Excel\AS4100 Verification.xlsx",
-        sheet_name="Is",
-        index_col=0,
-        usecols="B:AM",
-    )
-
-    return [(row.Index, row) for row in df.itertuples()]
-
-
-def data_PFCs():
-
-    df = pd.read_excel(
-        r"Excel\AS4100 Verification.xlsx",
-        sheet_name="PFCs",
-        index_col=0,
-        usecols="B:AN",
-    )
-
-    return [(row.Index, row) for row in df.itertuples()]
-
-
-def data_CHSs():
-
-    df = pd.read_excel(
-        r"Excel\AS4100 Verification.xlsx",
-        sheet_name="CHSs",
-        index_col=0,
-        usecols="B:V",
-    )
-
-    return [(row.Index, row) for row in df.itertuples()]
-
-
-def data_Welds():
-
-    df = pd.read_excel(
-        r"Excel\AS4100 Verification.xlsx",
-        sheet_name="Is",
-        index_col=0,
-        usecols="B:AL",
-    )
-
-    df = df.drop(df[df["Fabrication Type"] != "Welded"].index)
-
-    return [(row.Index, row) for row in df.itertuples()]
-
-
-def data_α_vma():
-
-    df = pd.read_excel(
-        r"Excel\AS4100 Verification.xlsx",
-        sheet_name="Non-Uniform Shear Factor",
-        index_col=0,
-        usecols="B:E",
-    )
-
-    return [(row.Index, row) for row in df.itertuples()]
-
-
-def data_Boxes():
-
-    df = pd.read_excel(
-        r"Excel\AS4100 Verification.xlsx",
-        sheet_name="CustomBoxes",
-        index_col=0,
-        usecols="B:Z",
-    )
-
-    return [(row.Index, row) for row in df.itertuples()]
-
-
-@pytest.mark.parametrize("name, data", data_Is() + data_PFCs() + data_Boxes())
+@pytest.mark.parametrize("name, data", data_Is + data_PFCs + data_Boxes)
 def test_s5_11_4_V_w_Generic(name, data):
 
     Aw = data.Aw
@@ -96,7 +23,7 @@ def test_s5_11_4_V_w_Generic(name, data):
     assert isclose(expected, actual)  # use default isclose tolerance (rel_tol of 1e-9)
 
 
-@pytest.mark.parametrize("name, data", data_CHSs())
+@pytest.mark.parametrize("name, data", data_CHSs)
 def test_s5_11_4_V_w_CHS(name, data):
 
     Ae = data.Ag
@@ -108,7 +35,7 @@ def test_s5_11_4_V_w_CHS(name, data):
     assert isclose(expected, actual)  # use default isclose tolerance (rel_tol of 1e-9)
 
 
-@pytest.mark.parametrize("name, data", data_Is() + data_PFCs() + data_Boxes())
+@pytest.mark.parametrize("name, data", data_Is + data_PFCs + data_Boxes)
 def test_s5_11_5_α_v(name, data):
 
     d_p = data.d1
@@ -121,7 +48,7 @@ def test_s5_11_5_α_v(name, data):
     assert isclose(expected, actual)  # use default isclose tolerance (rel_tol of 1e-9)
 
 
-@pytest.mark.parametrize("name, data", data_α_vma())
+@pytest.mark.parametrize("name, data", data_α_vma)
 def test_s5_11_3_α_vma(name, data):
 
     f_vm = data.fvm
@@ -133,7 +60,7 @@ def test_s5_11_3_α_vma(name, data):
     assert isclose(expected, actual)  # use default isclose tolerance (rel_tol of 1e-9)
 
 
-@pytest.mark.parametrize("name, data", data_Welds())
+@pytest.mark.parametrize("name, data", data_Welds)
 def test_s5_11_4_V_w_Weld(name, data):
 
     v_w = data.vw
@@ -146,7 +73,7 @@ def test_s5_11_4_V_w_Weld(name, data):
     assert isclose(expected, actual)  # use default isclose tolerance (rel_tol of 1e-9)
 
 
-@pytest.mark.parametrize("name, data", data_Is() + data_PFCs())
+@pytest.mark.parametrize("name, data", data_Is + data_PFCs)
 def test_s5_11_4_V_w_Interface(name, data):
 
     tw = data.tw  # web side of interface
@@ -160,7 +87,7 @@ def test_s5_11_4_V_w_Interface(name, data):
     assert isclose(expected, actual)  # use default isclose tolerance
 
 
-@pytest.mark.parametrize("name, data", data_Boxes())
+@pytest.mark.parametrize("name, data", data_Boxes)
 def test_s5_11_4_V_w_Interface_2(name, data):
 
     tw = data.tw * 2
