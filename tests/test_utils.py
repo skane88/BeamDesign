88173cb95c4f6002062839408549a37d86e7086f
skane88 <sean.kane@outlook.com.au>
2019-01-17 14:48:52 +1000

    Added test_utils file to load data from the spreadsheets.
    Uses the fact that imports are only carried out once to minimise the no.
    of calls to the spreadsheet to load the data once as module variables.

---
+++ tests/test_utils.py
@@ -0,0 +1,86 @@
+"""
+Includes some utility functions for testing.
+"""
+
+import pandas as pd
+
+def get_Is():
+
+    df = pd.read_excel(
+        r"Excel\AS4100 Verification.xlsx",
+        sheet_name="Is",
+        index_col=0,
+        usecols="B:AU",
+    )
+
+    return [(row.Index, row) for row in df.itertuples()]
+
+
+def get_PFCs():
+
+    df = pd.read_excel(
+        r"Excel\AS4100 Verification.xlsx",
+        sheet_name="PFCs",
+        index_col=0,
+        usecols="B:AV",
+    )
+
+    return [(row.Index, row) for row in df.itertuples()]
+
+
+def get_CHSs():
+
+    df = pd.read_excel(
+        r"Excel\AS4100 Verification.xlsx",
+        sheet_name="CHSs",
+        index_col=0,
+        usecols="B:AD",
+    )
+
+    return [(row.Index, row) for row in df.itertuples()]
+
+
+def get_Welds():
+
+    df = pd.read_excel(
+        r"Excel\AS4100 Verification.xlsx",
+        sheet_name="Is",
+        index_col=0,
+        usecols="B:AU",
+    )
+
+    df = df.drop(df[df["Fabrication Type"] != "Welded"].index)
+
+    return [(row.Index, row) for row in df.itertuples()]
+
+
+def get_α_vma():
+
+    df = pd.read_excel(
+        r"Excel\AS4100 Verification.xlsx",
+        sheet_name="Non-Uniform Shear Factor",
+        index_col=0,
+        usecols="B:E",
+    )
+
+    return [(row.Index, row) for row in df.itertuples()]
+
+
+def get_Boxes():
+
+    df = pd.read_excel(
+        r"Excel\AS4100 Verification.xlsx",
+        sheet_name="CustomBoxes",
+        index_col=0,
+        usecols="B:AH",
+    )
+
+    return [(row.Index, row) for row in df.itertuples()]
+
+data_Is = get_Is()
+data_PFCs = get_PFCs()
+data_CHSs = get_CHSs()
+data_Welds = get_Welds()
+data_α_vma = get_α_vma()
+data_Boxes = get_Boxes()
+data_AllSections = data_Is + data_PFCs + data_CHSs + data_Boxes
