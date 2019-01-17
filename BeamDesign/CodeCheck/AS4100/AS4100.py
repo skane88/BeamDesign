d2fde3ca56b54d402182f37b50909387bd290df2
skane88 <sean.kane@outlook.com.au>
2019-01-17 14:47:49 +1000

    Added tension methods to AS4100.

--- BeamDesign/CodeCheck/AS4100/AS4100.py
+++ BeamDesign/CodeCheck/AS4100/AS4100.py
@@ -10,6 +10,12 @@ from BeamDesign.CodeCheck.CodeCheck import CodeCheck
 
 class AS4100(CodeCheck):
     def __init__(self, *, beam: Beam = None, section=None, **kwargs):
+        """
+
+        :param beam:
+        :param section:
+        :param kwargs:
+        """
 
         super().__init__(beam=beam, section=section)
 
@@ -22,8 +28,62 @@ class AS4100(CodeCheck):
         return super().get_section(position=position)
 
     @staticmethod
-    def S7_tension(Ag: float, An: float, fy: float, fu: float, kt: float):
+    def s7_1_Nt(
+        *, Ag: float, An: float, fy: float, fu: float, kt: float, αu: float = 0.85
+    ):
+        """
+        Calculates the tension capacity of a section according to AS4100 S7.1.
 
-        raise NotImplemented
+        :param Ag: Gross area of a section in m².
+        :param An: Net area of the section in m², allowing for holes as required
+            by AS4100.
+        :param fy: The yield strength of the section in Pa. If different components
+            have different yield strengths the minimum strength of the section
+            should be used. Where a section has a significantly different strength
+            the result may be too conservative and this function may not be
+            appropriate (i.e. a 250 grade web and a 450 grade flange) - more
+            detailed analysis (FEA modelling etc.) may be required.
+        :param fu: The ultimate strength of the section in Pa.
+        :param kt: The connection efficiency factor / eccentric connection factor
+            as per AS4100.
+        :param αu: A factor for the uncertainty in ultimate srength as per AS4100 S7.2.
+            By default 0.85. Note that AS4100 does not provide a variable name for this
+            value so αu is used, consistent with other uses of α in AS4100.
+        :return:
+        """
 
+        return min(
+            AS4100.s7_2_Nty(Ag=Ag, fy=fy), AS4100.s7_2_Ntu(An=An, fu=fu, kt=kt, αu=αu)
+        )
 
+    @staticmethod
+    def s7_2_Nty(*, Ag: float, fy: float) -> float:
+        """
+        Calculates the yield capacity of a member but does not include the capacity
+        reduction factor.
+
+        :param Ag: Gross area of a section in m².
+        :param fy: Yield strength of a section in Pa.
+        :return: Returns the yielding capacity of the member in N.
+        """
+
+        return Ag * fy
+
+    @staticmethod
+    def s7_2_Ntu(*, An: float, fu: float, kt: float, αu: float = 0.85) -> float:
+        """
+        Calculates the ultimate fracture capacity of a section and includes the
+        additional uncertainty factor from AS4100.
+
+        :param An: Net area of the section in m², allowing for holes as required
+            by AS4100.
+        :param fu: The ultimate strength of the section in Pa.
+        :param kt: The connection efficiency factor / eccentric connection factor
+            as per AS4100.
+        :param αu: A factor for the uncertainty in ultimate srength as per AS4100 S7.2.
+            By default 0.85. Note that AS4100 does not provide a variable name for this
+            value so αu is used, consistent with other uses of α in AS4100.
+        :return: Returns the ultimate fracture capacity in N.
+        """
+
+        return An * fu * kt * αu
