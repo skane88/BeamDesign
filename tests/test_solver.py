2018ef6a0b74769844e1d092999af5cf4b94a809
skane88 <sean.kane@outlook.com.au>
2019-02-08 10:32:10 +1000

    Added a test file for test_solver and tests for the sign function.
    NOTE: the test for the sign function against negative numbers requires
    hypothesis 4.5.2 or later.

---
+++ tests/test_solver.py
@@ -0,0 +1,33 @@
+"""
+This file contains tests for the solver modules intended to solve for utilisations etc.
+"""
+
+from hypothesis import given
+from hypothesis.strategies import floats
+
+from beamdesign.utility.solvers import sign
+
+
+@given(floats(min_value=0))
+def test_sign_pos(n):
+    """
+    Test the sign function. Uses hypothesis to cover all the positive numbers.
+
+    :param n: The number to test.
+    """
+
+    assert sign(n) == 1
+
+
+@given(floats(max_value=0, exclude_max=True))
+def test_sign_neg(n):
+    """
+    Test the sign function against negative numbers. Uses hypothesis to cover all the
+    negative numbers, excludes 0.
+
+    NOTE: requires hypothesis 4.5.2 or later.
+
+    :param n: The number to test.
+    """
+
+    assert sign(n) == -1
