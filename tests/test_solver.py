d03e06dcce7d4fa5ff6d217a1bafaa1866f6a19e
skane88 <sean.kane@outlook.com.au>
2019-02-09 14:16:00 +1000

    Improved tests for the solvers
    Added checks for non-linear equations.

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -2,7 +2,9 @@
 This file contains tests for the solver modules intended to solve for utilisations etc.
 """
 
-from math import isclose, inf, nan, isinf, isnan
+from math import isclose, isinf, sqrt, exp, sin, cos, pi
+
+from pytest import mark
 
 from hypothesis import given, assume
 from hypothesis.strategies import floats
@@ -10,8 +12,12 @@ from hypothesis.strategies import floats
 from beamdesign.utility.solvers import bisection, secant
 
 
-@given(floats(allow_infinity=False, allow_nan=False))
-def test_bisection_1(n):
+@given(
+    n=floats(allow_infinity=False, allow_nan=False),
+    x_low=floats(allow_infinity=False, allow_nan=False),
+    x_high=floats(allow_infinity=False, allow_nan=False),
+)
+def test_bisection_1(n, x_low, x_high):
     """
     Test of the bisection method.
 
@@ -21,13 +27,45 @@ def test_bisection_1(n):
     def func(x):
         return x + n
 
-    try:
-        x, i = bisection(func=func)
+    assume(not isinf(func(x_low)))
+    assume(not isinf(func(x_high)))
+    # eliminates a source of floating point errors - there is a guard in the actual
+    # method itself that catches this and raises a ValueError.
 
-        assert isclose(x, -n, abs_tol=1e-9)
-    except ValueError:
-        # have placed a number of asserts in code to catch errors related to inf, nan
-        assert True
+    assume(func(x_low) * func(x_high) < 0.0)
+    # basic assumption of the bisection method.
+
+    x, i = bisection(func=func)
+
+    assert isclose(x, -n, abs_tol=1e-9)
+
+
+@mark.parametrize(
+    "func, x_low, x_high, root",
+    [
+        (lambda x: x ** 2 - 3, -2, -1, -sqrt(3)),
+        (lambda x: x ** 2 - 3, 1, 2, sqrt(3)),
+        (
+            lambda x: exp(-x) * (3.2 * sin(x) - 0.5 * cos(x)),
+            3,
+            4,
+            3.2965893955137342207663577582129950752528,
+        ),
+        (lambda x: sin(x), -4, -3, -pi),
+        (lambda x: sin(x), 3, 4, pi),
+        (lambda x: cos(x), -2, -1, -pi/2),
+        (lambda x: cos(x), 1, 2, pi/2),
+    ],
+)
+def test_bisection_2(func, x_low, x_high, root):
+    """
+    Test the bisection method against some other function types.
+    """
+
+    x, i = bisection(func=func, x_low=x_low, x_high=x_high)
+
+    assert isclose(x, root, abs_tol=1e-8)
+    # note tol of 1e-8 - solver not quite finding some roots
 
 
 @given(floats(allow_infinity=False, allow_nan=False))
@@ -85,3 +123,30 @@ def test_secant_2(n, x_low, x_high):
     x, i, b = secant(func=func, x_low=x_low, x_high=x_high)
 
     assert isclose(x, -n, abs_tol=1e-9)
+
+@mark.parametrize(
+    "func, x_low, x_high, root",
+    [
+        (lambda x: x ** 2 - 3, -2, -1, -sqrt(3)),
+        (lambda x: x ** 2 - 3, 1, 2, sqrt(3)),
+        (
+            lambda x: exp(-x) * (3.2 * sin(x) - 0.5 * cos(x)),
+            3,
+            4,
+            3.2965893955137342207663577582129950752528,
+        ),
+        (lambda x: sin(x), -4, -3, -pi),
+        (lambda x: sin(x), 3, 4, pi),
+        (lambda x: cos(x), -2, -1, -pi/2),
+        (lambda x: cos(x), 1, 2, pi/2),
+    ],
+)
+def test_secant_3(func, x_low, x_high, root):
+    """
+    Test the bisection method against some other function types.
+    """
+
+    x, i, b = secant(func=func, x_low=x_low, x_high=x_high)
+
+    assert isclose(x, root, abs_tol=1e-8)
+    # note tol of 1e-8 - solver not quite finding some roots
