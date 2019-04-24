c4a2aa38a90b4ca2cda704107e3218f77a6e4c88
skane88 <sean.kane@outlook.com.au>
2019-04-24 20:56:47 +1000

    Initial work to try out __init__ usage to shorten imports.
    Started with as4100 and codecheck.

---
+++ beamdesign/codecheck/__init__.py
@@ -0,0 +1,3 @@
+from .codecheck import CodeCheck
+from .as4100 import AS4100
+from .as4100 import AS4100Section
---
+++ beamdesign/codecheck/as4100/__init__.py
@@ -0,0 +1,2 @@
+from .as4100 import AS4100
+from .as4100_sect_props import AS4100Section
